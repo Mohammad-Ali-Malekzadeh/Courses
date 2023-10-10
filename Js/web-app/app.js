9654c0e0962e44fc3a65e8edcb62d76cecb95b97
Mohammad-Ali-Malekzadeh <75009952+Mohammad-Ali-Malekzadeh@users.noreply.github.com>
2023-10-10 19:15:11 +0330

    Until Episode 41 js (Front Cast - Massod Sadri)

--- Js/web-app/app.js
+++ Js/web-app/app.js
@@ -1,5 +1,59 @@
-document.querySelector('#add-product-form').addEventListener('submit', (e) => {
+const products = [{
+    title: 'Book 1',
+    exist: true
+}, {
+    title: 'Book 2',
+    exist: false
+}, {
+    title: 'Book 3',
+    exist: true
+}, {
+    title: 'Book 4',
+    exist: false
+}]
+
+const filters = {
+    searchItem: '',
+    availableProducts: false
+}
+
+const renderProducts = function (products, filters) {
+    let filteredProducts = products.filter(function (item) {
+        return item.title.toLowerCase().includes(filters.searchItem.toLowerCase())
+    })
+    filteredProducts = filteredProducts.filter(function (item) {
+        if (filters.availableProducts) {
+            return item.exist
+        } else {
+            return true
+        }
+    })
+    document.querySelector('#products').innerHTML = ''
+    filteredProducts.forEach(item => {
+        const productEl = document.createElement('p')
+        productEl.textContent = item.title
+        document.querySelector('#products').appendChild(productEl)
+    })
+}
+
+renderProducts(products, filters)
+
+document.querySelector('#search-products').addEventListener('input', function (e) {
+    filters.searchItem = e.target.value
+    renderProducts(products, filters)
+})
+
+document.querySelector('#add-product-form').addEventListener('submit', function (e) {
     e.preventDefault()
-    console.log(e.target.elements.ProductTitle.value)
-    e.target.elements.ProductTitle.value = ''
+    products.push({
+        title: e.target.elements.productTitle.value ,
+        exist: true
+    })
+    renderProducts(products, filters)
+    e.target.elements.productTitle.value = ''
+})
+
+document.querySelector('#available-products').addEventListener('change', function (e) {
+    filters.availableProducts = e.target.checked
+    renderProducts(products, filters)
 })
