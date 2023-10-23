9e0548c72aec2861a9373710ba609c2272601c36
Mohammad-Ali-Malekzadeh <75009952+Mohammad-Ali-Malekzadeh@users.noreply.github.com>
2023-10-23 22:06:42 +0330

    Until Episode 104 css (Front Cast - Massod Sadri)

--- Js/web-app/app.js
+++ Js/web-app/app.js
@@ -1,59 +1,14 @@
-const products = [{
-    title: 'Book 1',
-    exist: true
-}, {
-    title: 'Book 2',
-    exist: false
-}, {
-    title: 'Book 3',
-    exist: true
-}, {
-    title: 'Book 4',
-    exist: false
-}]
-
-const filters = {
-    searchItem: '',
-    availableProducts: false
+/* const product = {
+    title: 'Book',
+    price: 79
 }
 
-const renderProducts = function (products, filters) {
-    let filteredProducts = products.filter(function (item) {
-        return item.title.toLowerCase().includes(filters.searchItem.toLowerCase())
-    })
-    filteredProducts = filteredProducts.filter(function (item) {
-        if (filters.availableProducts) {
-            return item.exist
-        } else {
-            return true
-        }
-    })
-    document.querySelector('#products').innerHTML = ''
-    filteredProducts.forEach(item => {
-        const productEl = document.createElement('p')
-        productEl.textContent = item.title
-        document.querySelector('#products').appendChild(productEl)
-    })
-}
+const productJSON = JSON.stringify(product)
 
-renderProducts(products, filters)
+localStorage.setItem('product', productJSON) */
 
-document.querySelector('#search-products').addEventListener('input', function (e) {
-    filters.searchItem = e.target.value
-    renderProducts(products, filters)
-})
+const productJSON = localStorage.getItem('product')
 
-document.querySelector('#add-product-form').addEventListener('submit', function (e) {
-    e.preventDefault()
-    products.push({
-        title: e.target.elements.productTitle.value ,
-        exist: true
-    })
-    renderProducts(products, filters)
-    e.target.elements.productTitle.value = ''
-})
+const product = JSON.parse(productJSON)
 
-document.querySelector('#available-products').addEventListener('change', function (e) {
-    filters.availableProducts = e.target.checked
-    renderProducts(products, filters)
-})
+console.log(`Title: ${product.title} - Price: ${product.price}`)
