4d3c79abc8de8e31f9bbf2888be90e3bb7fa4b77
Mohammad-Ali-Malekzadeh <75009952+Mohammad-Ali-Malekzadeh@users.noreply.github.com>
2024-02-04 00:04:11 +0330

    Untill 1/2 Python-exercise-solution-course

--- Python-exercise-solution-course/Season1/my_answer.py
+++ Python-exercise-solution-course/Season1/my_answer.py
@@ -1,5 +1,6 @@
 select_ex = "n_" + input("Please Select one of the Exercises (1 - 12): ")
 
+
 def ex1():
     num = int(input("Please enter a number: "))
     if num % 2 == 0:
@@ -24,29 +25,48 @@ def ex3():
 
     print(max(numbers))
 
+
 def ex4():
     num = int(input("Please Enter a Number: "))
     if num < 0:
         print("Please Enter a Positive Number!!!")
         quit()
     result = 0
-    for i in range(1 , num + 1):
+    for i in range(1, num + 1):
         result += i
     print(result)
+
 
 def ex5():
     num = int(input("Please Enter a Number: "))
     print(len(str(num)))
 
+
 def ex6():
-    string = input('Please Enter Text: ')
-    reversed_str = ''
+    string = input("Please Enter Text: ")
+    reversed_str = ""
 
     for i in range(-1, -(len(string) + 1), -1):
         reversed_str += string[i]
 
     print(reversed_str)
-    
+
+
+def ex7():
+    string = input("Please Enter Text: ")
+    upper_count = 0
+    lower_count = 0
+    for i in string:
+        if i.isupper():
+            upper_count += 1
+        else:
+            lower_count += 1
+    print(
+        "Your Test have {upper} Upper Case and {lower} Lower Case.".format(
+            upper=upper_count, lower=lower_count
+        )
+    )
+
 exercises = {
     "n_1": ex1,
     "n_2": ex2,
@@ -54,8 +74,8 @@ exercises = {
     "n_4": ex4,
     "n_5": ex5,
     "n_6": ex6,
-    # "n_7": ex7,
-    # "n_8": ex8,
+    "n_7": ex7,
+    # "n_8": ex8,?
     # "n_8": ex9,
     # "n_8": ex10,
     # "n_8": ex11,
