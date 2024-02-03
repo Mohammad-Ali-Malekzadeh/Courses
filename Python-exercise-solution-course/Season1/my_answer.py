0994ade7f4be530fc1de0ba8ed38433a8672148f
Mohammad-Ali-Malekzadeh <75009952+Mohammad-Ali-Malekzadeh@users.noreply.github.com>
2024-02-03 21:54:34 +0330

    Untill 1/3 Python-exercise-solution-course

--- Python-exercise-solution-course/Season1/my_answer.py
+++ Python-exercise-solution-course/Season1/my_answer.py
@@ -1,18 +1,59 @@
 select_ex = "n_" + input("Please Select one of the Exercises (1 - 12): ")
 
 def ex1():
-    num = int(input('Please enter a number: '))
+    num = int(input("Please enter a number: "))
     if num % 2 == 0:
-        print('Your Number is is Odd!')
+        print("Your Number is is Odd!")
     else:
-        print('Your Number is Even!')
+        print("Your Number is Even!")
 
+
+def ex2():
+    num = int(input("Please Enter a Number: "))
+    while num <= 100:
+        print(num)
+        num += 1
+
+
+def ex3():
+    numbers = []
+    for i in range(0, 3):
+        numbers.append(
+            int(input("Please Enter your {num} Number: ".format(num=str(i + 1))))
+        )
+
+    print(max(numbers))
+
+def ex4():
+    num = int(input("Please Enter a Number: "))
+    if num < 0:
+        print("Please Enter a Positive Number!!!")
+        quit()
+    result = 0
+    for i in range(1 , num + 1):
+        result += i
+    print(result)
+
+def ex5():
+    num = int(input("Please Enter a Number: "))
+    print(len(str(num)))
+
+def ex6():
+    string = input('Please Enter Text: ')
+    reversed_str = ''
+
+    for i in range(-1, -(len(string) + 1), -1):
+        reversed_str += string[i]
+
+    print(reversed_str)
+    
 exercises = {
     "n_1": ex1,
-    # "n_2": ex2,
-    # "n_3": ex3,
-    # "n_5": ex5,
-    # "n_6": ex6,
+    "n_2": ex2,
+    "n_3": ex3,
+    "n_4": ex4,
+    "n_5": ex5,
+    "n_6": ex6,
     # "n_7": ex7,
     # "n_8": ex8,
     # "n_8": ex9,
